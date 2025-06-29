f35b41b7d19ff41b0b3e1b615a72816812818d68
nakulupp1l <uppalnakul14@gmail.com>
2025-06-29 19:13:41 +0530

    counter app using event listener

--- DOM/lec1/Counter_app/index.js
+++ DOM/lec1/Counter_app/index.js
@@ -1,11 +1,24 @@
 const val=document.querySelector('#counter');
-const increment=()=>{
+// const increment=()=>{
+//     let ans=parseInt(val.innerText);
+//     ans++;
+//     val.innerText=ans;
+// };
+// const decrement=()=>{
+//     let ans=parseInt(val.innerText);
+//     ans--;
+//     val.innerText=ans;
+// };
+
+let content1=document.querySelector('#inc');
+let x=content1.addEventListener('click',()=>{
     let ans=parseInt(val.innerText);
     ans++;
     val.innerText=ans;
-};
-const decrement=()=>{
+});
+let content2=document.querySelector('#dec');
+x=content2.addEventListener('click',()=>{
     let ans=parseInt(val.innerText);
     ans--;
     val.innerText=ans;
-};
+});
